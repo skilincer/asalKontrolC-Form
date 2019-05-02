2ac1b71b78c0e92f0933906b51e155514b80979e
skilincer <kilincer007@hotmail.com>
2019-05-02 22:18:51 +0300

    C#

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asalKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            int sayi = Convert.ToInt32(textBox1.Text);
+            for (int i = 2; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                sayac++;
+                break;
+                    //girilen sayının asallık kontrolü
+                }
+            }    
+                
+            if (sayac == 0)
+        	{   
+                 label2.Text = "sayi asaldır";
+	        }
+            else
+	        {
+                  label2.Text ="sayi asal degildir";
+	        }
+        }
+    }
+}
+
